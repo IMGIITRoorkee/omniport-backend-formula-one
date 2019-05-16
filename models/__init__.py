4458c7c20ab2f2c3bb65c3fbba75a2f9e84b6464
Dhruv Bhanushali <dhruv_b@live.com>
2019-05-16 22:00:15 +0530

    Move the base and generic models, and their serializers, to Formula 1

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+from formula_one.models.generics.contact_information import ContactInformation
+from formula_one.models.generics.location_information import LocationInformation
+from formula_one.models.generics.social_information import (
+    SocialInformation,
+    SocialLink,
+)
---
+++ serializers/generics/social_information.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+
+from formula_one.models import SocialLink, SocialInformation
+
+
+class SocialLinkSerializer(serializers.ModelSerializer):
+    """
+    Serializer for SocialLink objects
+    """
+
+    site_name = serializers.ReadOnlyField()
+    site_logo = serializers.ReadOnlyField()
+
+    class Meta:
+        """
+        Meta class for SocialLinkSerializer
+        """
+
+        model = SocialLink
+
+        exclude = [
+            'datetime_created',
+            'datetime_modified',
+        ]
+
+
+class SocialInformationSerializer(serializers.ModelSerializer):
+    """
+    Serializer for SocialInformation objects
+    """
+
+    links = SocialLinkSerializer(
+        many=True,
+    )
+
+    class Meta:
+        """
+        Meta class for SocialInformationSerializer
+        """
+
+        model = SocialInformation
+
+        exclude = [
+            'datetime_created',
+            'datetime_modified',
+            'entity_content_type',
+            'entity_object_id',
+        ]
