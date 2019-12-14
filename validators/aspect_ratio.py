7d32aea828b805d16a8666191fedf16e4d0aafb9
pradumangoyal <goyal.praduman611@gmail.com>
2019-12-14 20:39:04 +0530

    Add a confidenc argument in the AspectRatio validator to allow a range of aspect ratio

--- validators/aspect_ratio.py
+++ validators/aspect_ratio.py
@@ -10,15 +10,16 @@ class AspectRatioValidator:
     the field
     """
 
-    def __init__(self, aspect_ratio):
+    def __init__(self, aspect_ratio, confidence=0):
         """
         Initialise a callable instance of the class with the given parameters
         :param aspect_ratio: the ratio of the width of the image to its height
         """
 
         self.aspect_ratio = aspect_ratio
+        self.confidence = confidence
 
-    def check(self, value):
+    def check(self, value, confidence):
         """
         Check if the value meets all required criteria
         :param value: the value of the field
@@ -27,16 +28,17 @@ class AspectRatioValidator:
 
         width, height = get_image_dimensions(value)
         aspect_ratio = width / height
-        return aspect_ratio == self.aspect_ratio
+        return aspect_ratio >= self.aspect_ratio - self.confidence and \
+            aspect_ratio <= self.aspect_ratio + self.confidence
 
-    def __call__(self, value):
+    def __call__(self, value, confidence=0):
         """
         Call the check() function and raise an error if validation fails
         :param value: the value of the field being validated
         :raise: ValidationError, if the aspect ratio is incorrect
         """
 
-        if not self.check(value):
+        if not self.check(value, confidence):
             raise ValidationError(
                 'The aspect ratio of the uploaded file is incorrect. '
                 f'The aspect ratio should be {self.aspect_ratio}.'
