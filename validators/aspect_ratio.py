b27bd1b0d4c9c6919c40cd905d7d600f82daf95e
Praduman Goyal <goyal.praduman611@gmail.com>
2019-12-18 22:47:33 +0530

    Change `confidence` argument to `tolerance`

--- validators/aspect_ratio.py
+++ validators/aspect_ratio.py
@@ -10,16 +10,16 @@ class AspectRatioValidator:
     the field
     """
 
-    def __init__(self, aspect_ratio, confidence=0):
+    def __init__(self, aspect_ratio, tolerance=0):
         """
         Initialise a callable instance of the class with the given parameters
         :param aspect_ratio: the ratio of the width of the image to its height
         """
 
         self.aspect_ratio = aspect_ratio
-        self.confidence = confidence
+        self.tolerance = tolerance
 
-    def check(self, value, confidence):
+    def check(self, value, tolerance):
         """
         Check if the value meets all required criteria
         :param value: the value of the field
@@ -28,17 +28,17 @@ class AspectRatioValidator:
 
         width, height = get_image_dimensions(value)
         aspect_ratio = width / height
-        return aspect_ratio >= self.aspect_ratio - self.confidence and \
-            aspect_ratio <= self.aspect_ratio + self.confidence
+        return aspect_ratio >= self.aspect_ratio - self.tolerance and \
+            aspect_ratio <= self.aspect_ratio + self.tolerance
 
-    def __call__(self, value, confidence=0):
+    def __call__(self, value, tolerance=0):
         """
         Call the check() function and raise an error if validation fails
         :param value: the value of the field being validated
         :raise: ValidationError, if the aspect ratio is incorrect
         """
 
-        if not self.check(value, confidence):
+        if not self.check(value, tolerance):
             raise ValidationError(
                 'The aspect ratio of the uploaded file is incorrect. '
                 f'The aspect ratio should be {self.aspect_ratio}.'
