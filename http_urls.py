7b25d9ed363ce7ec7f0843d4aaec6a19956035ad
Ayush Jain <ayushjain20n@gmail.com>
2019-12-26 21:57:21 +0530

    Revert "Fetch contenttype objects for the selected model"
    This reverts commit 03cf9ff42b6bf3764c5b23e2c6fa421fa05cfb50.

--- http_urls.py
+++ http_urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 
-from formula_one.views.contenttype_object import ContentTypeObjectList
 from formula_one.views.csrf import EnsureCsrf
 from formula_one.views.hello import Hello
 from formula_one.views.manifest import Manifest
@@ -11,9 +10,4 @@ urlpatterns = [
     path('hello/', Hello.as_view(), name='hello'),
     path('ensure_csrf/', EnsureCsrf.as_view(), name='ensure_csrf'),
     path('manifest/', Manifest.as_view(), name='manifest'),
-    path(
-        'contenttype_object/<int:pk>/',
-        ContentTypeObjectList.as_view(),
-        name='contenttype_objects',
-    ),
 ]
