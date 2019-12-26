e16caa4c9a66db341bf8774d977a93220d64634a
Ayush Jain <ayushjain20n@gmail.com>
2019-12-26 21:57:21 +0530

    Revert "Break lines for consistency"
    This reverts commit 1b7bc74312700b1935756c06d94007913cf042fa.

--- admin/admin.py
+++ admin/admin.py
@@ -9,16 +9,10 @@ from omniport.admin.site import omnipotence
 
 # Register all non-swappable models
 
-omnipotence.register(
-    ContactInformation,
-    admin_form_dict['ContactInformation'],
-)
+omnipotence.register(ContactInformation, admin_form_dict['ContactInformation'])
 omnipotence.register(
     LocationInformation,
     admin_form_dict['LocationInformation'],
 )
-omnipotence.register(
-    SocialInformation,
-    admin_form_dict['SocialInformation'],
-)
+omnipotence.register(SocialInformation, admin_form_dict['SocialInformation'])
 omnipotence.register(SocialLink)
