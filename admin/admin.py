b7359fef60360487aea5a714fef2d897e122929b
Aman Sharma <mannu.poski10@gmail.com>
2019-12-27 19:43:10 +0530

    Trigger a popup when a ContentType is selected

--- admin/admin.py
+++ admin/admin.py
@@ -4,21 +4,11 @@ from formula_one.models import (
     SocialInformation,
     SocialLink,
 )
-from formula_one.forms.admin import admin_form_dict
 from omniport.admin.site import omnipotence
 
 # Register all non-swappable models
 
-omnipotence.register(
-    ContactInformation,
-    admin_form_dict['ContactInformation'],
-)
-omnipotence.register(
-    LocationInformation,
-    admin_form_dict['LocationInformation'],
-)
-omnipotence.register(
-    SocialInformation,
-    admin_form_dict['SocialInformation'],
-)
+omnipotence.register(ContactInformation)
+omnipotence.register(LocationInformation)
+omnipotence.register(SocialInformation)
 omnipotence.register(SocialLink)
--- static/formula_one/js/contenttype-object.js
+++ static/formula_one/js/contenttype-object.js
@@ -2,21 +2,14 @@ django.jQuery(document).ready(function() {
     django.jQuery('#id_entity_content_type').change(function(event) {
         // ID of the ContentType instance
         const conttenttypeId = event.target.value
-        // ID of the dropdown list to be populated
-        const entityContentObject = '#id_entity_content_object'
-        // Reset list on change of ContentType instance
-        django.jQuery(entityContentObject).html('')
 
-        // GET request to fetch all objects in the ContentType instance
-        django.jQuery.ajax({
-            type: 'GET',
-            url: `/contenttype_object/${conttenttypeId}/`,
-            success: function(data) {                
-                django.jQuery.each(data, function(_, obj) {
-                    django.jQuery('<option/>').val(obj.value).text(obj.label)
-                    .appendTo(entityContentObject)
-                })
-            },
-        })
+        window.open(
+            `/contenttype_object/${conttenttypeId}/`,"","width=900,height=700"
+        )
     })
 })
+
+function getId(id) {
+    window.opener.document.getElementById('id_entity_object_id').value = id
+    window.close()
+}
