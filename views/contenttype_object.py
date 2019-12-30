17b418c0dfae2c54108417389ad412e51b5aa0cb
Aman Sharma <mannu.poski10@gmail.com>
2019-12-30 23:20:21 +0530

    Paginate the results

--- views/contenttype_object.py
+++ views/contenttype_object.py
@@ -1,8 +1,9 @@
-from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.contenttypes.models import ContentType
+from django.core.paginator import Paginator
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
-from django.contrib.contenttypes.models import ContentType
+from django.views.generic.base import TemplateView
 
 
 class ContentTypeObjectList(LoginRequiredMixin, TemplateView):
@@ -12,10 +13,11 @@ class ContentTypeObjectList(LoginRequiredMixin, TemplateView):
     """
 
     template_name = 'formula_one/contenttype_object_list.html'
+    paginate_by = 10
 
     def get_context_data(self, **kwargs):
         """
-        Return the list of all objects inside the ContentType model class and 
+        Return the list of all objects inside the ContentType model class and
         the name of the ContentType model
         :return: context for template
         """
@@ -25,7 +27,7 @@ class ContentTypeObjectList(LoginRequiredMixin, TemplateView):
         try:
             contenttype_object_type = ContentType.objects.get_for_id(pk)
         except ObjectDoesNotExist:
-            raise Http404("ContentType does not exist")
+            raise Http404('ContentType does not exist')
 
         contenttype_objects = (
             contenttype_object_type.model_class().objects.all()
@@ -35,14 +37,18 @@ class ContentTypeObjectList(LoginRequiredMixin, TemplateView):
         for contenttype_object in contenttype_objects:
             contenttype_objects_choices.append(
                 {
-                    "id": contenttype_object.pk,
-                    "label": str(contenttype_object),
+                    'id': contenttype_object.pk,
+                    'label': str(contenttype_object),
                 },
             )
 
+        paginator = Paginator(contenttype_objects_choices, self.paginate_by)
+        page = self.request.GET.get('page')
+        contenttype_object_list = paginator.get_page(page)
+
         context = {
-            "contenttype_objects_list": contenttype_objects_choices,
-            "model": contenttype_object_type.model,
+            'contenttype_object_list': contenttype_object_list,
+            'model': contenttype_object_type.model,
         }
 
         return context
